e17e6bfca1f20353a3fe9f9aedeec86b29b86f11
dongwushenhua <976680003@qq.com>
2022-01-18 16:52:15 +0800

    1.18大修

---
+++ src/request/api/admin/tag.js
@@ -0,0 +1,27 @@
+/**
+ * article模块接口列表
+ */
+
+import base from "../../base"; // 导入接口域名列表
+import axios from "axios"; // 导入http中创建的axios实例
+
+
+const article = {
+  // 新闻列表
+  articleList () {
+    return axios.get(`${base.sq}/topics`);
+  },
+  // 新闻详情,演示
+  articleDetail (id, params) {
+    return axios.get(`${base.sq}/topic/${id}`, {
+      params: params
+    });
+  },
+  // post提交
+  login (params) {
+    return axios.post(`${base.sq}/accesstoken`, qs.stringify(params));
+  }
+  // 其他接口…………
+}
+
+export default article;
---
+++ src/request/base.js
@@ -0,0 +1,9 @@
+/**
+ * 接口域名的管理
+ */
+const base = {
+  sq: 'https://xxxx111111.com/api/v1',
+  bd: 'http://xxxxx22222.com/api'
+}
+
+export default base;
